0d87180a2586ef64f1c9b480f537cbe610a7c1ec
Raul Val <raulval@outlook.com>
2023-08-17 11:30:54 -0300

    feat: add get project handler logic

--- handler/projectHandler/getProject.go
+++ handler/projectHandler/getProject.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/raulval/gofreela/schemas"
 )
 
 func GetProjectHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Project",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	project := schemas.Project{}
+
+	if err := db.First(&project, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "project not found")
+		return
+	}
+
+	sendSuccess(ctx, project)
 }
