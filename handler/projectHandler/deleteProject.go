ff8d98aafdb0443734b8b671052660144c8ac3b6
Raul Val <raulval@outlook.com>
2023-08-17 11:42:51 -0300

    feat: add delete project handler logic

--- handler/projectHandler/deleteProject.go
+++ handler/projectHandler/deleteProject.go
@@ -4,10 +4,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/raulval/gofreela/schemas"
 )
 
 func DeleteProjectHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Project",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	project := schemas.Project{}
+
+	if err := db.First(&project, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "project not found")
+		return
+	}
+
+	if err := db.Delete(&project).Error; err != nil {
+		logger.Errorf("error deleting project: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error deleting project")
+		return
+	}
+
+	sendSuccess(ctx, project)
 }
