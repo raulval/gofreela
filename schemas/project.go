a977a78096ee1c3214ffb4a0baad46304fbb2661
Raul Val <raulval@outlook.com>
2023-08-17 10:40:45 -0300

    refactor: change deadline type and add struct response in schema

--- schemas/project.go
+++ schemas/project.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -9,8 +11,22 @@ type Project struct {
     Title           string
     Client          string
     Description     string
-    Deadline        string
+    Deadline        time.Time
     Status          string
     Value           float64
     IsPaid 					bool
+}
+
+type ProjectResponse struct {
+  ID              uint   `json:"id"`
+  Title           string `json:"title"`
+  Client          string `json:"client"`
+  Description     string `json:"description"`
+  Deadline        time.Time `json:"deadline"`
+  Status          string `json:"status"`
+  Value           float64 `json:"value"`
+  IsPaid 					bool `json:"isPaid"`
+  CreatedAt       time.Time `json:"createdAt"`
+  UpdatedAt       time.Time `json:"updatedAt"`
+  DeletedAt       time.Time `json:"deletedAt,omitempty"`
 }
