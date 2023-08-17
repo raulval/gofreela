7bad597219412e919ad24ee9f2389e2736072721
Raul Val <raulval@outlook.com>
2023-08-17 16:14:10 -0300

    feat: add list projects handler logic

--- handler/projectHandler/listProjects.go
+++ handler/projectHandler/listProjects.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/raulval/gofreela/schemas"
 )
 
 func ListProjectsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Projects",
-	})
+	projects := []schemas.Project{}
+
+	if err := db.Find(&projects).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing projects")
+		return
+	}
+
+	sendSuccess(ctx, projects)
 }
